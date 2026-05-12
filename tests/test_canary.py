20fe05b9d3432f8be4df51251c9dff714aae0d3b
Andrey Pogorely <a.pogorely@pm.me>
2026-05-12 10:50:40 -0700

    feat(canary): add farewell(name) function
    Closes #1

--- tests/test_canary.py
+++ tests/test_canary.py
@@ -1,5 +1,9 @@
-from src.canary import hello
+from src.canary import farewell, hello
 
 
 def test_hello():
     assert hello("world") == "hello, world"
+
+
+def test_farewell():
+    assert farewell("world") == "goodbye, world"
